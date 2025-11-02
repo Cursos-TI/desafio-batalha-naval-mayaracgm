ae7e3c38ae7030e60571c364e3b974a55047df4a
mayaracgm <mayara.machuca@gmail.com>
2025-11-02 21:11:19 +0000

    Nível aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    #include <stdio.h>
-
-    // Declaração da matriz bidimensional para o tabuleiro 5x5
-    int tabuleiro[5][5] = {0};
-
-    // Posicionamento do navio vertical (tamanho 3)
-    int coluna_vertical = 2; // coluna fixa
-    for (int linha = 0; linha < 3; linha++) {
-        tabuleiro[linha][coluna_vertical] = 1;
-        printf("Navio Vertical - Parte %d: (%d, %d)\n", linha + 1, linha, coluna_vertical);
-    }
-
-    // Posicionamento do navio horizontal (tamanho 4)
-    int linha_horizontal = 4; // linha fixa
-    for (int coluna = 0; coluna < 4; coluna++) {
-        tabuleiro[linha_horizontal][coluna] = 1;
-        printf("Navio Horizontal - Parte %d: (%d, %d)\n", coluna + 1, linha_horizontal, coluna);
-    }
-
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
+// Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+
+
+// Tamanho do tabuleiro
+#define TAMANHO 10
+
+// Valor que representa navio
+#define NAVIO 3
+
+int main() { // Nível Aventureiro - Dois navios na diagonal
+
+    // Declaração do tabuleiro 10x10, inicializado com água (0)
+    int tabuleiro[TAMANHO][TAMANHO] = {0};
+
+    // ----------------------------
+    // Navio Diagonal 1 - Principal (tabuleiro[i][i])
+    // Começa em (1,1) e vai até (3,3)
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[1 + i][1 + i] = NAVIO;
+    }
+
+    // ----------------------------
+    // Navio Diagonal 2 - Secundária (tabuleiro[i][9 - i])
+    // Começa em (0,9) e vai até (2,7)
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[i][9 - i] = NAVIO;
+    }
+
+    // ----------------------------
+    // Exibição do Tabuleiro
+    // ----------------------------
+    printf("Tabuleiro com dois navios diagonais:\n\n");
+    for (int i = 0; i < TAMANHO; i++) {
+        for (int j = 0; j < TAMANHO; j++) {
+            printf("%2d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
